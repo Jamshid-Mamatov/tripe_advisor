ef2b1783fc4b1aaeb1846b8f0a0c4b4c235a1c60
DevJamshid <mamatovjamshid605@gmail.com>
2023-02-08 15:15:07 +0500

    information of first page is scraped

---
+++ restaurants/restaurants/spiders/restaurant.py
@@ -0,0 +1,24 @@
+import scrapy
+import base64
+
+class RestaurantSpider(scrapy.Spider):
+    name = "restaurant"
+    allowed_domains = ["tripadvisor.com"]
+    start_urls = ["https://www.tripadvisor.com/Restaurants-g32655-Los_Angeles_California.html"]
+
+    def parse(self, response):
+        
+        for link in response.xpath("//a[@class='Lwqic Cj b']/@href").getall():
+           
+            yield scrapy.Request(response.urljoin(link), self.additional_page)
+
+
+    
+    def additional_page(self,response):
+        
+        yield{
+            "name": response.xpath("//h1[@class='HjBfq']/text()").get(),
+            "location" : response.xpath("//a[@class='AYHFM']/text()").get(),
+            "tel_nomer" : response.xpath("//a[@class='BMQDV _F G- wSSLS SwZTJ']/text()").get(),
+            "restaurant_link":base64.b64decode(response.xpath("//a[@class='YnKZo Ci Wc _S C AYHFM']").attrib['data-encoded-url'])[4:]
+        }
